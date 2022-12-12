30fb67d4e097d4648e009dba454277eee82e982e
k2gutierrez <cchimal.gutierrez@gmail.com>
2022-12-12 12:53:39 -0600

    React Redux Forms Revisited

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Footer from './FooterComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
 
 const mapStateToProps = state => {
   return {
@@ -22,7 +23,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch) => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => { dispatch(fetchDishes())}
+  fetchDishes: () => { dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 })
 
 class Main extends Component {
@@ -52,7 +54,7 @@ class Main extends Component {
     const DishWithId = ({match}) => {
 
       return(
-        <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+        <DishDetail dish={this.props.dishes .dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
               dishesLoading={this.props.dishes.isLoading}
               dishesErrMess={this.props.dishes.errMess}
               comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
@@ -76,7 +78,7 @@ class Main extends Component {
             <Route path="/home" component={ HomePage } />
             <Route exact path="/menu" component={ () => <Menu dishes={this.props.dishes } /> } />
             <Route path="/menu/:dishId" component={ DishWithId } />
-            <Route exact path='/contactus' component={ Contact } />
+            <Route exact path='/contactus' component={ () => <Contact resetFeedbackForm={this.props.resetFeedbackForm} /> } />
             <Route exact path='/aboutus' component={ AboutUs } />
             <Redirect to="/home" /> 
           </Switch>
